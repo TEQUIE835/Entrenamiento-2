9cb3553cf2d8771f7650281b390816e6858629d1
TEQUIE835 <david123orjuela@gmail.com>
2025-04-28 11:32:38 -0500

    entrenamiento

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Entrenamiento-2
+
+Ejercicios propuestos por mi TL para entrenar en python
+
+
+# 1-Determinar el estado de aprobacion
+a. pedir una calificacion del 1 al 100
+b. verificar si esta aprobado o no
+
+# 2-calcular promedio
+a. pedir una lista de notas
+b.Calcular y mostrar el promedio de las notas
+# 3-Contar calificaciones mayores
+a. Pedir un valor al usuario
+b. contar cuantas notas son mayores en una lista de notas
---
+++ entrenamiento.py
@@ -0,0 +1,109 @@
+import sys
+
+def trytrue():
+    while True:
+        try:
+            trytrue=int(input("\nIngrese una nota del 1 al 100: \n"))
+            if 0<=trytrue<=100:
+                return trytrue
+            else:
+                print("\nIngrese un numero valido")
+        except ValueError:
+            print("\nIngrese un numero valido")
+
+while True:
+    try:
+        n=int(input("Cuantas notas desea ingresar?\n"))
+        break
+    except ValueError:
+        print("\nIngrese una cantidad valida")
+
+notas = []
+for i in range(n):
+    a=i+1
+    while True:
+        try:
+            nota= int(input(f"\nIngrese nota {a} (1-100)\n"))
+            if 0<=nota<=100:
+                break
+            else:
+                print("\nIngrese un numero valido")
+        except:
+            print("\nIngrese una nota valida")
+    notas.append(nota)
+
+
+#verificar si aprobo
+
+def aprobado():
+
+    notapr=trytrue()
+    
+    if 100>=notapr>=60:
+        print("\nFelicidades, has aprobado")
+    elif 60>notapr>=0:
+        print("\nNo has aprobado, esfuerzate mas")
+
+
+#Calcular promedio
+
+def promedio():
+    prom = 0
+    for i in range(len(notas)):
+        prom+= notas[1]
+    prom = prom/n
+    print(f"\nTu promedio es: {prom}")
+
+
+#Contar numeros mayores
+def mayores():
+    notam=trytrue()
+    cont=0
+    for i in range(len(notas)):
+        if notam<notas[i]:
+            cont += 1
+    
+    print(f"\nHay {cont} notas mayores")
+
+
+def salir():
+    print("\nNos vemos...")
+    sys.exit()
+
+
+def mostrarmenu():
+    print("\nQue quieres hacer?")
+    print("1. Verificar aprobacion")
+    print("2. Calcular promedio")
+    print("3. Contar numeros mayores")
+    print("4. Mostrar notas")
+    print("5. Salir")
+
+
+while True:
+    mostrarmenu()
+    while True:
+        try:
+            opc=int(input("\nIngrese su opcion: "))
+            if 1<=opc<=5:
+                break
+            else:
+                print("Ingrese una opcion valida")
+        except ValueError:
+            print("Ingrese una opcion valida")
+
+    if opc==1:
+        aprobado()
+    elif opc==2:
+        promedio()
+    elif opc==3:
+        mayores()
+    elif opc==4:
+        print("\nTus notas son: ")
+        for i in range(len(notas)):
+            print(notas[i])
+            
+    elif opc==5:
+        salir()
+    else:
+        print("Hubo un error")
